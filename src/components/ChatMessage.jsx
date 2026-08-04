d5a451b71f5c23961ae987294cbc7d131d991d36
zaynemundo <zayne.mayo@gmail.com>
2026-08-05 01:21:45 +0400

    style: swap LinkedIn glyph for jam icon set svg

--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -243,8 +243,8 @@ export default function ChatMessage({ message, onRunInCanvas, onReviseCode, onRe
         return (
           <a key={i} href={href} target="_blank" rel="noopener noreferrer" className="markdown-link">
             {isLinkedIn && (
-              <svg className="linkedin-icon" viewBox="0 0 24 24" width="14" height="14" role="img" aria-label="LinkedIn">
-                <path fill="#0a66c2" d="M20.45 20.45h-3.55v-5.57c0-1.33-.03-3.04-1.85-3.04-1.86 0-2.14 1.45-2.14 2.94v5.67H9.35V9h3.41v1.56h.05c.48-.9 1.64-1.85 3.37-1.85 3.6 0 4.27 2.37 4.27 5.46v6.28zM5.34 7.43a2.06 2.06 0 1 1 0-4.12 2.06 2.06 0 0 1 0 4.12zM7.12 20.45H3.56V9h3.56v11.45zM22.22 0H1.77C.79 0 0 .77 0 1.72v20.56C0 23.23.79 24 1.77 24h20.45c.98 0 1.78-.77 1.78-1.72V1.72C24 .77 23.2 0 22.22 0z" />
+              <svg className="linkedin-icon" viewBox="-2 -2 24 24" width="14" height="14" role="img" aria-label="LinkedIn" xmlns="http://www.w3.org/2000/svg">
+                <path fill="#0a66c2" d="M19.959 11.719v7.379h-4.278v-6.885c0-1.73-.619-2.91-2.167-2.91-1.182 0-1.886.796-2.195 1.565-.113.275-.142.658-.142 1.043v7.187h-4.28s.058-11.66 0-12.869h4.28v1.824l-.028.042h.028v-.042c.568-.875 1.583-2.126 3.856-2.126 2.815 0 4.926 1.84 4.926 5.792zM2.421.026C.958.026 0 .986 0 2.249c0 1.235.93 2.224 2.365 2.224h.028c1.493 0 2.42-.989 2.42-2.224C4.787.986 3.887.026 2.422.026zM.254 19.098h4.278V6.229H.254v12.869z" />
               </svg>
             )}
             {linkMatch[1]}
